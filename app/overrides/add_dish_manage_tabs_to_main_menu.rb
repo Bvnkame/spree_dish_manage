94483011c99facdc181c57519ff56fa3cbe2b24b
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-03-30 12:05:59 +0700

    update

--- app/overrides/add_dish_manage_tabs_to_main_menu.rb
+++ app/overrides/add_dish_manage_tabs_to_main_menu.rb
@@ -1,8 +1,8 @@
-Deface::Override.new(:virtual_path => 'spree/admin/shared/_tabs',
-  :name => 'add_dish_menu_tabs',
-  :insert_before => "erb[silent]:contains('Spree::Admin::ReportsController')",
-  :text => "
-    <% if can? :admin, Spree::Product %>
-      <%= tab *Spree::BackendConfiguration::DISH_TABS, icon: 'shopping-cart' %>
-    <% end %>
-  ")
+# Deface::Override.new(:virtual_path => 'spree/admin/shared/_tabs',
+#   :name => 'add_dish_menu_tabs',
+#   :insert_before => "erb[silent]:contains('Spree::Admin::ReportsController')",
+#   :text => "
+#     <% if can? :admin, Spree::Product %>
+#       <%= tab *Spree::BackendConfiguration::DISH_TABS, icon: 'shopping-cart' %>
+#     <% end %>
+#   ")
